79464f0615a9060cbbd8445c0ccc4f168fb3b827
Islam Abdelmajeed <islam.abdelmajeed2@gmail.com>
2024-07-27 14:47:22 +0300

    Refactor Login Feature for Enhanced Functionality and Validation
    This commit introduces significant enhancements to the login feature, focusing on improving user input handling and validation. Key changes include:
    
    1. **Cubit State Management Integration**: Added form key and text editing controllers to the `LoginCubit` for better state management and form validation.
    
    2. **Form Validation and User Feedback**: Implemented a comprehensive form validation system with real-time feedback for password requirements, including lowercase letters, uppercase letters, numbers, special characters, and minimum length.
    
    3. **UI Refactoring**: Streamlined the login screen UI by removing redundant state management and integrating new form fields and validation widgets directly into the login process.
    
    4. **Bloc Listener for State Handling**: Introduced a `LoginBlocListener` to manage and display different states (loading, success, error) during the login process, enhancing user experience with immediate feedback.
    
    5. **New Widgets for Reusability**: Created new widgets (`EmailAndPassword`, `PasswordValidations`, `LoginBlocListener`) to encapsulate and reuse form fields, validation logic, and state handling, promoting cleaner and more maintainable code.
    
    These changes aim to improve the robustness and user-friendliness of the login process, ensuring that users receive clear and immediate feedback on their input validity.

--- lib/features/login/logic/cubit/login_cubit.dart
+++ lib/features/login/logic/cubit/login_cubit.dart
@@ -1,11 +1,16 @@
 import 'package:doc_doc/features/login/data/models/login_request_body.dart';
 import 'package:doc_doc/features/login/data/repo/login_repo.dart';
 import 'package:doc_doc/features/login/logic/cubit/login_state.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginCubit extends Cubit<LoginState> {
   final LoginRepo _loginRepo;
   LoginCubit(this._loginRepo) : super(const LoginState.initial());
+
+  final formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
   void emitLoginStates(LoginRequestBody loginRequestBody) async {
     emit(const LoginState.loading());
--- lib/features/login/login_screen.dart
+++ lib/features/login/login_screen.dart
@@ -1,22 +1,20 @@
-import 'package:doc_doc/core/theming/colors.dart';
 import 'package:doc_doc/core/theming/size_boxes.dart';
 import 'package:doc_doc/core/theming/styles.dart';
 import 'package:doc_doc/core/widgets/app_text_button.dart';
-import 'package:doc_doc/core/widgets/app_text_form_field.dart';
+import 'package:doc_doc/features/login/data/models/login_request_body.dart';
 import 'package:doc_doc/features/login/widgets/dont_have_account_text.dart';
+import 'package:doc_doc/features/login/widgets/email_and_password.dart';
 import 'package:doc_doc/features/login/widgets/terms_and_conditions_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'logic/cubit/login_cubit.dart';
+import 'widgets/login_bloc_listener.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
-
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
+class LoginScreen extends StatelessWidget {
+  LoginScreen({super.key});
   GlobalKey formKey = GlobalKey<FormState>();
+
   bool isObscureText = true;
 
   @override
@@ -31,34 +29,27 @@ class _LoginScreenState extends State<LoginScreen> {
               children: [
                 Text("Welcome Back!", style: TextStyles.font24BlueBold),
                 8.heightBox,
-                Text("We're excited to have you back, can't wait to see what you've been up to since you last logged in.", style: TextStyles.font14GrayRegular),
+                Text("We're excited to have you back, can't wait to see what you've been up to since you last logged in.",
+                    style: TextStyles.font14GrayRegular),
                 36.heightBox,
-                Form(
-                  key: formKey,
-                  child: Column(
-                    children: [
-                      AppTextFormField(hintText: "Email", validator: (value) => null),
-                      18.heightBox,
-                      AppTextFormField(
-                        hintText: "Password",
-                        validator: (value) => null,
-                        isObscureText: isObscureText,
-                        suffixIcon: GestureDetector(
-                            onTap: () {
-                              setState(() => isObscureText = !isObscureText);
-                            },
-                            child: Icon(isObscureText ? Icons.visibility : Icons.visibility_off)),
-                      ),
-                      24.heightBox,
-                      Align(alignment: Alignment.centerRight, child: Text("Forgot Password?", style: TextStyles.font13BlueRegular)),
-                      40.heightBox,
-                      AppTextButton(buttonText: "Login", textStyle: TextStyles.font16WhiteSemiBold, onPressed: () {}),
-                      16.heightBox,
-                      const TermsAndConditionsText(),
-                      60.heightBox,
-                      const DontHaveAccountText(),
-                    ],
-                  ),
+                Column(
+                  children: [
+                    const EmailAndPassword(),
+                    24.heightBox,
+                    Align(alignment: Alignment.centerRight, child: Text("Forgot Password?", style: TextStyles.font13BlueRegular)),
+                    40.heightBox,
+                    AppTextButton(
+                        buttonText: "Login",
+                        textStyle: TextStyles.font16WhiteSemiBold,
+                        onPressed: () {
+                          validateThenDoLogin(context);
+                        }),
+                    16.heightBox,
+                    const TermsAndConditionsText(),
+                    60.heightBox,
+                    const DontHaveAccountText(),
+                    const LoginBlocListener(),
+                  ],
                 ),
               ],
             ),
@@ -66,5 +57,13 @@ class _LoginScreenState extends State<LoginScreen> {
         ),
       ),
     );
+  }
+
+  void validateThenDoLogin(BuildContext context) {
+    if (context.read<LoginCubit>().formKey.currentState!.validate()) {
+      context.read<LoginCubit>().emitLoginStates(
+            LoginRequestBody(email: context.read<LoginCubit>().emailController.text, password: context.read<LoginCubit>().passwordController.text),
+          );
+    }
   }
 }
