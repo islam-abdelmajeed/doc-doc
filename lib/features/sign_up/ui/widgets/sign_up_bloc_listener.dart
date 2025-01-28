d73c787c2d47c2c9e700eb98bfc94501955231f7
Islam Abdelmajeed <islam.abdelmajeed2@gmail.com>
2025-01-28 12:39:38 +0200

    Refactor code for improved readability and update plugin imports

--- lib/features/sign_up/ui/widgets/sign_up_bloc_listener.dart
+++ lib/features/sign_up/ui/widgets/sign_up_bloc_listener.dart
@@ -68,29 +68,10 @@ class SignUpBlocListener extends StatelessWidget {
   void setupErrorState(BuildContext context, String error) {
     context.pop();
     showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        icon: const Icon(
-          Icons.error,
-          color: Colors.red,
-          size: 32,
-        ),
-        content: Text(
-          error,
-          style: TextStyles.font15DarkBlueMedium,
-        ),
-        actions: [
-          TextButton(
-            onPressed: () {
-              context.pop();
-            },
-            child: Text(
-              'Got it',
-              style: TextStyles.font14BlueSemiBold,
-            ),
-          ),
-        ],
-      ),
-    );
+        context: context,
+        builder: (context) => AlertDialog(
+            icon: const Icon(Icons.error, color: Colors.red, size: 32),
+            content: Text(error, style: TextStyles.font15DarkBlueMedium),
+            actions: [TextButton(onPressed: () => context.pop(), child: Text('Got it', style: TextStyles.font14BlueSemiBold))]));
   }
 }
